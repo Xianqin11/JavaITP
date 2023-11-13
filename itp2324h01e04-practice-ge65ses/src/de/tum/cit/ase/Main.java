17ca05682c19530d54dd3deb5146cb5ed4997798
Xianqin11 <150745802+Xianqin11@users.noreply.github.com>
2023-11-13 23:48:53 +0100

    Add files via upload

---
+++ itp2324h01e04-practice-ge65ses/src/de/tum/cit/ase/Main.java
@@ -0,0 +1,31 @@
+package de.tum.cit.ase;
+
+public class Main {
+
+    //TODO 1: Create the missing classes
+
+    //TODO 2: Write constructors for the classes Pumpkin,Ghost and Candle
+
+    //TODO 3: Add getters and setters for the classes Pumpkin,Ghost and Candle
+
+    //TODO 4: Add methods for the classes Pumpkin,Ghost and Candle
+
+    //TODO 5: Implement the JackOLantern constructor
+
+    //TODO 6: Calculate the weight of the ghost and implement the methods in the Ghost Class
+
+    //TODO 7: Calculate the weight of the pumpkin
+
+    //TODO 8: Calculate the weight of the candle
+
+    //TODO 9: Weigh your lantern
+
+    //TODO 10: Live Demo of the Lantern
+
+    //TODO 11: Add static Methods
+
+    public static void main(String[] args) {
+
+    }
+
+}
